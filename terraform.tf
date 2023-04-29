06917aff291719981f0bfe4bd71c3fa378f9850f
D3vNinja <101419916+D3vNinja@users.noreply.github.com>
2023-04-28 22:14:54 -0500

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket          = "tf-statefile-bucket"
+    key             = "tf-statefile-bucket/terraform.tf.state"
+    region          = "us-east-1"
+    dynamodb_table  = "state-locking"
+  }
+}
