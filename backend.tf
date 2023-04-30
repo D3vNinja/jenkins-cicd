0353d800df2c5c499f5e04f1b85787485b591f66
D3vNinja <101419916+D3vNinja@users.noreply.github.com>
2023-04-30 12:14:01 -0500

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket   = "tf-statefile-bucket"
-    key      = "tf-statefile-bucket/terraform.tf.state"
-    region   = "us-east-1"
-    dynamodb = "state-locking"
-  }
-}
